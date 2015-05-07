8d67ff5e97405288097d46e07b62a2fa5f903274
James Zuccollo <james.zuccollo@reform.co.uk>
2015-05-07 16:53:40 +0100

    Add tests for monthly and annual ONS downloads.

--- pyscraper/tests/scrape_test.py
+++ pyscraper/tests/scrape_test.py
@@ -1,32 +1,44 @@
 import nose
 import pandas as pd
-from pydatafuncs import scrape
+from pyscraper import scrape
 
 # Check that all major scrape calls are returning the right data type
 
 
-def test_from_ONS_returns_dataframe():
+def test_from_ONS_quarterly_returns_dataframe():
     "Test that scrape.from_ONS returns a pandas dataframe"
     test_frame = scrape.from_ONS('qna', ['YBHA', 'ABMI'], 'Q')
-    nose.tools.assert_true(type(test_frame) == pd.core.frame.DataFrame)
+    nose.tools.assert_true(isinstance(test_frame, pd.core.frame.DataFrame))
+
+
+def test_from_ONS_annual_returns_dataframe():
+    "Test that scrape.from_ONS returns a pandas dataframe"
+    test_frame = scrape.from_ONS('qna', ['YBHA', 'ABMI'], 'A')
+    nose.tools.assert_true(isinstance(test_frame, pd.core.frame.DataFrame))
+
+
+def test_from_ONS_monthly_returns_dataframe():
+    "Test that scrape.from_ONS returns a pandas dataframe"
+    test_frame = scrape.from_ONS('lms', ['MGSX'], 'M')
+    nose.tools.assert_true(isinstance(test_frame, pd.core.frame.DataFrame))
 
 
 def test_from_BoE_returns_dataframe():
     "Test that scrape.from_BoE returns a pandas dataframe"
     test_frame = scrape.from_BoE(
         ['LPMAUZI', 'LPMAVAA'], datefrom=pd.datetime(2007, 8, 1))
-    nose.tools.assert_true(type(test_frame) == pd.core.frame.DataFrame)
+    nose.tools.assert_true(isinstance(test_frame, pd.core.frame.DataFrame))
 
 
 def test_from_weo_returns_panel():
     "Test that scrape.from_IMF returns a pandas panel on the WEO dataset"
     test_frame = scrape.from_IMF(
         'weo', series=['GGSB_NPGDP', 'GGX_NGDP'], countries=['United Kingdom'])
-    nose.tools.assert_true(type(test_frame) == pd.core.panel.Panel)
+    nose.tools.assert_true(isinstance(test_frame, pd.core.panel.Panel))
 
 
 def test_from_pubfin_returns_panel():
     "Test that scrape.from_IMF returns a pandas panel on the Public Finances dataset"
     test_frame = scrape.from_IMF(
         'pubfin', series=['rev', 'prim_exp'], countries=['United Kingdom'])
-    nose.tools.assert_true(type(test_frame) == pd.core.panel.Panel)
+    nose.tools.assert_true(isinstance(test_frame, pd.core.panel.Panel))
